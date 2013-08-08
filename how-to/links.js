00aeaffcfc797c61f103935891da24f2bbb2970a
Emmanuel Flores Elias <efeapple@gmail.com>
2013-08-08 14:40:57 +0200

    what

---
+++ how-to/links.js
@@ -0,0 +1,32 @@
+//history
+http://www.youtube.com/watch?v=SAc0vQCC6UQ
+
+//hardware
+http://www.youtube.com/watch?v=bu1Trc0FxSQ
+https://vimeo.com/68530396
+https://vimeo.com/69841474
+https://vimeo.com/53478267
+
+//graphics
+http://www.youtube.com/watch?v=64TcBiqmVko
+http://www.youtube.com/watch?v=3IYme6zJr1o
+http://www.youtube.com/watch?v=dYWit6zTpzY
+https://vimeo.com/24741361
+
+//sound
+//soooo cheesy
+http://www.youtube.com/watch?v=BanBzvuDtIY
+
+//AI
+http://www.youtube.com/watch?v=NG31UChnNfc
+
+
+//Hardware accelerated with node
+http://nodebots.io
+https://nodejsmodules.org/tags/hardware
+http://kyungw00k.github.io/nodeconf.kr-2012/#/
+http://voodootikigod.com/nodebots-the-rise-of-js-robotics/
+https://github.com/rwldrn/johnny-five
+http://nodecopter.com
+https://github.com/felixge/node-ar-drone
+https://github.com/leapmotion/leapjs
